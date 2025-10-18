d18d921b89229abacbf600ccac7af8e02808133a
vextrbot-star <vextrbot@gmail.com>
2025-10-19 00:46:13 +0500

    chore: produce standalone Next output for Netlify plugin

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@ module.exports = {
   i18n: {
     locales: ['en'],
     defaultLocale: "en",
-  }
+  },
+  // Produce a standalone server build so Netlify's Next plugin can package
+  // server artifacts reliably (creates .next/standalone)
+  output: 'standalone',
 }
