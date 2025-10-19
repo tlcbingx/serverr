c1f15c94b0f904d54a278a8358c20807f1e59418
vextrbot-star <vextrbot@gmail.com>
2025-10-19 13:55:50 +0500

    Добавлен Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,15 @@ import './style.css'
 
 import { GlobalProvider } from '../global-context'
 import { NextIntlProvider } from 'next-intl'
+import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function MyApp({ Component, pageProps }) {
   return (
     <NextIntlProvider messages={pageProps?.messages}>
       <GlobalProvider>
         <Component {...pageProps} />
+        <Analytics />
+        <SpeedInsights />
       </GlobalProvider>
     </NextIntlProvider>
   )
